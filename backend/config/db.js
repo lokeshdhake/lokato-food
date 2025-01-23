6f4193333fcf22482065daca77cd08b1c3ab578c
lokeshdhake <109684470+lokeshdhake@users.noreply.github.com>
2025-01-23 19:52:52 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDb = async() => {
-    await mongoose.connect('mongodb+srv://prydator:Prydator8983@lokato.wbccv.mongodb.net/lokato').then(()=>console.log("DB connected"));
+    await mongoose.connect('').then(()=>console.log("DB connected"));
 }
